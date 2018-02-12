75744407a58d460d36d18dab46f973d96c5504c4
Arun Gadag <arun.gadag@awnics.com>
2018-02-12 17:56:43 +0530

    added more test cases

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mm-node
+# sequelize-test
 
 A starter boilerplate for NodeJS and Express
 
@@ -7,36 +7,35 @@ A starter boilerplate for NodeJS and Express
 **Note** This project requires node v4.x.x or higher and npm 2.14.7 but in order to be able to take advantage of the complete functionality we **recommend node >=v6.5.0 and npm >=3.10.3**.
 
 ```
-git clone https://github.com/AWNICS/mm-node.git
-cd mm-node
+git clone https://github.com/arun-awnics/sequelize-test.git
+cd sequelize-test
 
 # install project's dependencies
 npm install
 
 # watches your files for any change and restarts the server
 npm start
+
+# run test cases
+npm test
 ```
+
+**Note** Server needs to be running before running the test case.
 
 ## Configuration
 
 ```
 # configuring env
-mm-node/.env.dev
+sequelize-test/.env.dev
 
 # configuring MySql
-mm-node/src/util/conn.mysql.js
-
-# configuring MongoDB
-mm-node/src/util/conn.mysql.js
+sequelize-test/src/util/conn.mysql.js
 
 # configuring app
-mm-node/src/config/app.config.js
+sequelize-test/src/config/app.config.js
 
 # configuring log4js
-mm-node/src/config/log4js.json & mm-node/src/config/log4js.config.js
-
-# configuring swagger
-mm-node/src/config/swagger.config.js
+sequelize-test/src/config/log4js.json & sequelize-test/src/config/log4js.config.js
 ```
 
 ## License
--- src/test/user.test.js
+++ src/test/user.test.js
@@ -8,45 +8,33 @@ var should = chai.should();
 var userService = new UserService();
 
 describe('userDao', function() {
-    it('Get all users', async function() {
-        await userService.getAll((result) => {
-            log.info('result ', result);
-        });
-    });
 
-    it('Get all users', async function() {
-        await userService.getAll((result) => {
-            log.info('result ', result);
+    for (var i = 0; i < 1000; i++) {
+        it('creates a user', function(done) {
+            this.timeout(1000);
+            var user = {
+                id: null,
+                name: 'abc',
+                email: 'abc@gmail.com',
+                phoneNo: '9999999999',
+                picUrl: '',
+                description: 'Testing...',
+                status: '',
+                waitingTime: 10,
+                rating: 4
+            };
+            userService.insert(user, (res) => {
+                log.info('user value after insert: ' + JSON.stringify(res));
+            });
+            setTimeout(done, 500);
         });
-    });
+    }
 
-    it('Get all users', async function() {
-        await userService.getAll((result) => {
-            log.info('result ', result);
+    for (var i = 0; i < 50; i++) {
+        it('Get all users', async function() {
+            await userService.getAll((result) => {
+                log.info('result ', result);
+            });
         });
-    });
-
-    it('Get all users', async function() {
-        await userService.getAll((result) => {
-            log.info('result ', result);
-        });
-    });
-
-    it('Get all users', async function() {
-        await userService.getAll((result) => {
-            log.info('result ', result);
-        });
-    });
-
-    it('Get all users', async function() {
-        await userService.getAll((result) => {
-            log.info('result ', result);
-        });
-    });
-
-    it('Get all users', async function() {
-        await userService.getAll((result) => {
-            log.info('result ', result);
-        });
-    });
+    }
 });
